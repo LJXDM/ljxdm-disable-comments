e333a7a2a6b891cfd1f23e142b6f72cf641a3b5c
ac <ac@ljx.uk>
2023-11-09 11:07:48 +0000

    1.0.1 refactor

--- ljxdm-disable-comments.php
+++ ljxdm-disable-comments.php
@@ -1,78 +1,93 @@
 <?php
 /**
- * Plugin Name: LJXDM Disable Comments
- * Plugin URI: https://github.com/LJXDM/ljxdm-disable-comments
- * Description: Disable all comments and trackback features.
- * Version: 0.1
+ * Plugin Name: Simple Disable Comments
+ * Description: Simply disables comments. No marketing, no invasive menus, no hassle.
  * Author: ljxdm
- * Author URI: https://lojinx.digital
+ * Author URI: https://ljx.uk
+ * Version: 1.0.1
+ * Plugin Slug: ljxdm-disable-comments
  * License: GPLv3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-defined('ABSPATH') or die('No.');
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
 
-$ljxdm_disable_comments = new ljxdmDisableComments;
+define( 'LJXDM_DISABLE_COMMENTS_VERSION', '1.0.1' );
 
-class ljxdmDisableComments {
+class LJXDM_Disable_Comments {
 
+	private static $instance;
+	protected $plugin_file;
+	protected $plugin_basename;
 
-	public function __construct() {		
-		add_action('admin_init', array($this, 'ljxdm_disable_comments_post_types'));
-		add_action('admin_init', array($this, 'ljxdm_disable_comments_metabox'));
-		add_action('admin_menu', array($this, 'ljxdm_disable_comments_admin_menu'));
-		add_action('admin_init', array($this, 'ljxdm_disable_comments_admin_menu_redirect'));
-		add_action('init', array($this, 'ljxdm_disable_comments_admin_bar'));
-		add_filter('comments_open', array($this, 'ljxdm_disable_comments_status'), 20, 2);
-		add_filter('pings_open', array($this, 'ljxdm_disable_comments_status'), 20, 2);
-		add_filter('comments_array', array($this, 'ljxdm_disable_and_hide_comments'), 10, 2);
-		}
+	public function __construct() {
+		$this->plugin_file     = __FILE__;
+		$this->plugin_basename = plugin_basename( $this->plugin_file );
 
-	// disable post comments & trackbacks in post types.
-	public static function ljxdm_disable_comments_post_types() {
+		add_action( 'admin_init', array( &$this, 'redirect_admin_menu' ) );
+		add_action( 'admin_init', array( &$this, 'remove_meta_box' ) );
+		add_action( 'admin_init', array( &$this, 'remove_post_types_support' ) );
+		add_action( 'admin_menu', array( &$this, 'remove_admin_menu' ) );
+		add_action( 'wp_before_admin_bar_render', array( &$this, 'remove_admin_bar_menu' ) );
+
+		add_filter( 'comments_array', array( &$this, 'empty_array' ), 10, 2 );
+		add_filter( 'comments_open', array( &$this, 'false' ), 20, 2 );
+		add_filter( 'pings_open', array( &$this, 'false' ), 20, 2 );
+		add_action( 'pre_comment_on_post', array( &$this, 'disable_post_pre_comment' ) );
+	}
+
+	public function false() {
+		return false;
+	}
+
+	public function empty_array( $comments ) {
+		return array();
+	}
+
+	public function remove_post_types_support() {
+
 		$post_types = get_post_types();
 
-		foreach($post_types as $post_type) {
-			if(post_type_supports($post_type, 'comments')) {
-				remove_post_type_support($post_type, 'comments');
-				remove_post_type_support($post_type, 'trackbacks');
+		foreach ( $post_types as $post_type ) {
+			if ( post_type_supports( $post_type, 'comments' ) ) {
+				remove_post_type_support( $post_type, 'comments' );
+				remove_post_type_support( $post_type, 'trackbacks' );
 			}
 		}
 	}
 
-	// remove comments metabox
-	public static function ljxdm_disable_comments_metabox() {
-		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+	public function remove_admin_menu() {
+		remove_menu_page( 'edit-comments.php' );
 	}
 
-	// remove from admin menu
-	public static function ljxdm_disable_comments_admin_menu() {
-		remove_menu_page('edit-comments.php');
-	}
+	public function redirect_admin_menu() {
 
-	// redirect admin link, just in case
-	public static function ljxdm_disable_comments_admin_menu_redirect() {
 		global $pagenow;
-		if ('edit-comments.php' === $pagenow) {
-			wp_redirect( admin_url());
-			exit;
+
+		if ( 'edit-comments.php' === $pagenow ) {
+			wp_safe_redirect( admin_url() );
+			exit();
 		}
 	}
 
-	// remove from admin bar.
-	public static function ljxdm_disable_comments_admin_bar() {
-		if (is_admin_bar_showing()) {
-			remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-		}
-	}
-	// Close comments on the front-end.
-	public static function ljxdm_disable_comments_status() {
-		return false;
+	public function remove_meta_box() {
+
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
 	}
 
-	// hide existing comments.
-	public static function ljxdm_disable_and_hide_comments($comments) {
-		$comments = array();
-		return $comments;
+	public function remove_admin_bar_menu() {
+
+		global $wp_admin_bar;
+
+		$wp_admin_bar->remove_menu( 'comments' );
+	}
+
+	public function disable_post_pre_comment() {
+
+		wp_die( 'No comments' );
 	}
 }
+
+$ljxdm_disable_comments = new LJXDM_Disable_Comments();
